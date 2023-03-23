f6bdba98c3e821c4ab43b8769a270544116682de
LughsClues <cuberfreak@gmail.com>
2023-03-23 14:30:24 +0100

    Test #place_piece

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,1 +1,16 @@
 require_relative '../lib/connect_four'
+
+describe ConnectFour do
+  describe '#place_piece' do
+    context 'when a piece and a column is given' do
+      subject(:place_game) { described_class.new }
+
+      it 'updates the game board hash' do
+        place_game.place_piece('X', 6)
+        game_board = place_game.get_instance_variable(:@game_board)
+        expect(game_board[:col6][0]).to eq('X')
+      end
+    end
+  end
+
+end
