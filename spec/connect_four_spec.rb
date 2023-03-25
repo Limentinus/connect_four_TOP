10ea0815a620ca08e4cb2f6209aababf3560153f
LughsClues <cuberfreak@gmail.com>
2023-03-25 14:49:52 +0100

    Test for filled column

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -11,6 +11,22 @@ describe ConnectFour do
         expect(game_board[:col6][0]).to eq('X')
       end
     end
+
+    context 'when a piece is already in the column' do
+      filled_board = {col1: ['X', '', '', '', '', ''],
+                      col2: ['O', 'X', 'O', '', '', ''],
+                      col3: ['', '', '', '', '', ''],
+                      col4: ['', '', '', '', '', ''],
+                      col5: ['', '', '', '', '', ''],
+                      col6: ['', '', '', '', '', ''],
+                      col7: ['', '', '', '', '', '']}
+      subject(:filled_game) { described_class.new(filled_board) })}
+      it 'updates the place above the full space' do
+        filled_game.place.piece(:col1, 'O')
+        game_board = filled_game.instance_variable_get(:@game_board)
+        expect(game_board[:col1][1]).to eq('O')
+      end
+    end
   end
 
 end
